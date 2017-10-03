8cbf5350b32f353503c1205f812cd81ad35868c2
constantin_k <strongtigerman@gmail.com>
2017-10-03 17:45:47 +0300

    method GetTaskStatus added

---
+++ src/Mappers/TaskStatusMapper.cs
@@ -0,0 +1,35 @@
+﻿using Morph.Server.Sdk.Dto;
+using Morph.Server.Sdk.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morph.Server.Sdk.Mappers
+{
+    internal static class TaskStatusMapper
+    {
+        public static Model.TaskStatus MapFromDto(TaskStatusDto dto)
+        {
+            return new Model.TaskStatus()
+            {
+                TaskName = dto.TaskName,
+                StatusText = dto.StatusText,
+                TaskState = Parse(dto.Status),
+                IsRunning = dto.IsRunning
+            };
+        }
+
+        private static TaskState Parse(string value)
+        {            
+            TaskState status;
+            if(value != null &&  Enum.TryParse(value, true, out status))
+            {
+                return status;
+            }
+            throw new Exceptions.ParseResponseException("Unable to parse " + value + " as valid TaskState");
+        }
+        
+    }
+}
---
+++ src/Model/TaskStatus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morph.Server.Sdk.Model
+{
+    /// <summary>
+    /// Server status
+    /// </summary>
+    public class TaskStatus
+    {
+        public TaskState TaskState { get; set; }
+        public string TaskName { get; set; }
+        public string StatusText { get; set; }        
+        public bool IsRunning { get; set; }
+
+    }
+
+
+    public enum TaskState
+    {
+        Idle,
+        Scheduled,
+        Running,
+        Stopping,
+        Disabled,
+        Failed,
+    }
+
+
+
+}
