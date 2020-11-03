917486c619b4cf34126e8b6cd5e63d159cd47dcd
constantin_k <strongtigerman@gmail.com>
2020-11-03 22:28:46 +0200

    ServerStreamWrite  fix

--- src/Helper/ContiniousSteamingContent.cs
+++ src/Helper/ContiniousSteamingContent.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
@@ -11,30 +12,35 @@ namespace Morph.Server.Sdk.Helper
         /// <summary>
         ///  message type for processing
         /// </summary>
-        internal enum MessageType
+        private enum MessageType
         {
             /// <summary>
             /// no new messages
             /// </summary>
             None,
+
             /// <summary>
             /// push new stream to server
             /// </summary>
             NewStream,
+
             /// <summary>
             /// Close connection
             /// </summary>
-            CloseConnection
+            CloseConnection,
+
+            Exception
         }
 
         private const int DefBufferSize = 4096;
         private readonly CancellationToken mainCancellation;
 
-        
+
         /// <summary>
         /// cross-thread flag, that new message need to be processed
         /// </summary>
         volatile SemaphoreSlim hasData = new SemaphoreSlim(0, 1);
+
         /// <summary>
         /// cross-thread flag that new message has been processed
         /// </summary>
@@ -44,28 +50,36 @@ namespace Morph.Server.Sdk.Helper
         /// stream to process
         /// </summary>
         volatile Stream _stream;
+
         /// <summary>
         /// Message to process
         /// </summary>
         volatile MessageType _currentMessage = MessageType.None;
-        private CancellationToken cancellationToken;
 
-        internal async Task WriteStreamAsync( Stream stream, CancellationToken cancellationToken)
+        private CancellationToken streamWritercancellationToken;
+
+        private volatile Exception _serializeException = null;
+
+        internal async Task WriteStreamAsync(Stream stream, CancellationToken cancellationToken)
         {
-            if(_currentMessage != MessageType.None)
+            if (_currentMessage != MessageType.None)
             {
-                throw new System.Exception("Another message is processing by the ContiniousSteamingHttpContent handler. ");
+                throw new System.Exception(
+                    "Another message is processing by the ContiniousSteamingHttpContent handler. ");
             }
 
             this._stream = stream;
-            this.cancellationToken = cancellationToken;
+            this.streamWritercancellationToken = cancellationToken;
             this._currentMessage = MessageType.NewStream;
 
             // set flag that new data has been arrived
             hasData.Release(1); //has data->1         
             // await till all data will be send by another thread. Another thread will trigger dataProcessed semaphore
             await dataProcessed.WaitAsync(Timeout.Infinite, cancellationToken);
-       
+            if (_currentMessage == MessageType.Exception && _serializeException != null)
+            {
+                throw _serializeException;
+            }
         }
 
         internal void CloseConnetion()
@@ -79,51 +93,74 @@ namespace Morph.Server.Sdk.Helper
                 // wait until it has been processed
                 dataProcessed.Wait(5000);
             }
-            
         }
 
         public ContiniousSteamingHttpContent(CancellationToken mainCancellation)
         {
             this.mainCancellation = mainCancellation;
-            
         }
+
         protected override async Task SerializeToStreamAsync(Stream stream, TransportContext context)
         {
             var buffer = new byte[DefBufferSize];
             bool canLoop = true;
             while (canLoop)
             {
-                
                 // await new data
-                await hasData.WaitAsync(Timeout.Infinite, mainCancellation);
-                // data has been arrived. check _currentMessage field
-                switch (this._currentMessage) {
-                    // upload stream 
-                    case MessageType.NewStream:
-                    using (this._stream)
+                await hasData.WaitAsync(Timeout.Infinite, mainCancellation).ConfigureAwait(false);
+                try
+                {
+                    // data has been arrived. check _currentMessage field
+                    switch (this._currentMessage)
                     {
-                        int bytesRead;
-                        while ((bytesRead = await _stream.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false)) != 0)
-                        {
-                            await stream.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
-                        }
+                        // upload stream 
+                        case MessageType.NewStream:
+                            using (this._stream)
+                            {
+                                int bytesRead;
+                                while ((bytesRead = await _stream
+                                    .ReadAsync(buffer, 0, buffer.Length, streamWritercancellationToken)
+                                    .ConfigureAwait(false)) != 0)
+                                {
+                                    await stream.WriteAsync(buffer, 0, bytesRead, streamWritercancellationToken)
+                                        .ConfigureAwait(false);
+                                }
+                            }
+                            break;
+                        case MessageType.CloseConnection:
+                            // close loop. dataProcessed flag will be triggered at the end of this function.
+                            canLoop = false;
 
-                        // set that data has been processed
-                        dataProcessed.Release(1);
 
-                    }; break;
-                    case MessageType.CloseConnection:
-                        // close loop. dataProcessed flag will be triggered at the end of this function.
-                        canLoop = false;
-                      
-                        break;
-
+                            break;
+                    }
                 }
-                this._currentMessage = MessageType.None;              
-                
-            }
-            dataProcessed.Release(1);
+                catch (Exception ex)
+                {
+                    if (ex is OperationCanceledException && !streamWritercancellationToken.IsCancellationRequested)
+                    {
+                        // http timeout reached, override to timeout exception.
+                        _serializeException = new TimeoutException("Request timeout.");
+                    }
+                    else
+                    {
+                        _serializeException = ex;
+                    }
 
+                    _currentMessage = MessageType.Exception;
+                }
+                finally
+                {
+                    // set message to None
+                    if (_currentMessage != MessageType.Exception)
+                    {
+                        this._currentMessage = MessageType.None;
+                    }
+
+                    // set that data has been processed
+                    dataProcessed.Release(1);
+                }
+            }
         }
 
         protected override bool TryComputeLength(out long length)
--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("72ecc66f-62fe-463f-afad-e1ff5cc19cd9")]
 
 
-[assembly: AssemblyVersion("1.4.2.1")]
-[assembly: AssemblyFileVersion("1.4.2.1")]
+[assembly: AssemblyVersion("1.4.3.0")]
+[assembly: AssemblyFileVersion("1.4.3.0")]
